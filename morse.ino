8f18eef81e817ccb1a0d10bd02b1822e02f430bc
Lily <lolpo12345@outlook.com>
2022-03-22 22:05:30 +0300

    Initialize necessary arrays

--- morse.ino
+++ morse.ino
@@ -1,3 +1,10 @@
+int const MORSE_PIN = 13;
+String const MESSAGE = "sos";
+String MORSE;
+
+char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+String morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
