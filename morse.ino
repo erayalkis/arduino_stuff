8445611cf80b1bce094b380aaaebcec20de50533
Lily <lolpo12345@outlook.com>
2022-03-22 22:07:27 +0300

    Finish code

--- morse.ino
+++ morse.ino
@@ -1,10 +1,4 @@
 int const MORSE_PIN = 13;
-String const MESSAGE = "sos";
-String MORSE;
-
-char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-String morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
-int const MORSE_PIN = 13;
 String const MESSAGE = "your mother";
 String MORSE;
 
@@ -25,6 +19,30 @@ void dash() {
   delay(250);
 }
 
+String morsify(String input) {
+  String morse_string = "";
+  
+  for(int i = 0; i < input.length(); i++) {
+    if(input[i] == ' ') {
+      continue;
+    }
+    
+    int charIdx;
+    
+    for(int j = 0; j < sizeof(alphabet); j++) {
+       if(alphabet[j] == input[i]) {
+        charIdx = j;
+       }
+    }
+
+    morse_string.concat(morse[charIdx]);
+    morse_string.concat(" ");
+  }
+
+  Serial.println(morse_string);
+  return morse_string;
+}
+
 void read_morse(String morse_code) {
   
   for(int i = 0; i < morse_code.length(); i++) {
@@ -42,16 +60,22 @@ void read_morse(String morse_code) {
   }
   
 }
-
+// the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(MORSE_PIN, OUTPUT);
+  Serial.begin(9600);
+  MORSE = morsify(MESSAGE);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+// digitalWrite(MORSE_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+// delay(500);                       // wait for a second
+// digitalWrite(MORSE_PIN, LOW);    // turn the LED off by making the voltage LOW
+//  delay(500);    // wait for a second
+
+  read_morse(MORSE);
+
+  delay(1500);
 }
